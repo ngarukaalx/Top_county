94a51a9f1995df214f787ea1429bdefc39202a05
ngarukaalx <hiramkabuagi@gmail.com>
2024-05-27 06:29:24 +0000

    static

--- web_dynamic/front.py
+++ web_dynamic/front.py
@@ -10,7 +10,24 @@ cache_id = str(uuid.uuid4())
 
 @app.route('/live', strict_slashes=False)
 def test():
+    """test server"""
     return jsonify({"status": "fine"})
+
+@app.route('/home', strict_slashes=False)
+def home():
+    """serves the home page"""
+    return render_template('homepage.html', cache_id=cache_id)
+
+
+@app.route('/county', strict_slashes=False)
+def county():
+    """serves county"""
+    return render_template('county.html', cache_id=cache_id)
+
+@app.route('/user', strict_slashes=False)
+def user():
+    """servers user page"""
+    return render_template('user_page.html', cache_id=cache_id)
 
 
 if __name__ == "__main__":
